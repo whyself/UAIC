8fb1a4c57d61e5d6ec0de9d8307174153f78bad8
whyself <1158820305@qq.com>
2025-12-07 17:35:36 +0800

    change port

--- main.py
+++ main.py
@@ -66,4 +66,4 @@ app.add_middleware(
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8001, reload=True)
